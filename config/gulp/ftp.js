0ab5ce3da65c63022017465310aca43e11c16d16
Artem <artiom.ygryumov@gmail.com>
2022-08-05 16:51:34 +0300

    Главная страница с адаптивом, кроме пагинации на главном слайдере

--- config/gulp/ftp.js
+++ config/gulp/ftp.js
@@ -3,8 +3,8 @@ import vinylFTP from 'vinyl-ftp'
 export default function ftp() {
 	const ftpConnect = vinylFTP.create({
 		host: "37.140.192.24", // Адрес FTP сервера
-		user: "u1326657_test", // Имя пользователя
-		password: "yF3rN6dA4rwK2p", // Пароль
+		user: "u1326657_bigtork", // Имя пользователя
+		password: "dH7xW1sY9m", // Пароль
 		parallel: 5 // Кол-во одновременных потоков
 	});
 
--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,20 +1,58 @@
-// import Swiper, { Navigation, Pagination } from "swiper";
+import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from "swiper";
 
-// const mainSwiper = new Swiper("[data-swiper=main]", {
-//     modules: [ Navigation, Pagination ],
+const optFourthCards = {
+	modules: [Navigation, Pagination],
 
-// 	slidesPerView: 1,
-// 	spaceBetween: 24,
-// 	loop: true,
+	breakpoints: {
+		1200: {
+			slidesPerView: 4,
+			spaceBetween: 24,
+		},
+		900: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		},
+		550: {
+			slidesPerView: 2,
+			spaceBetween: 16,
+		},
+		0: {
+			slidesPerView: 1,
+			spaceBetween: 16,
+		},
+	},
+}
 
-//     navigation: {
-//         nextEl: ".main-swiper__arrow.is-next",
-//         prevEl: ".main-swiper__arrow.is-prev",
-//     },
+const caseSwiper = new Swiper("[data-swiper=case]", {
+	...optFourthCards,
 
-// 	pagination: {
-// 		el: '.main-swiper__pagin',
-// 		type: 'bullets',
-// 		clickable: true,
-// 	},
-// });
+	navigation: {
+		nextEl: ".s-case__arrow.is-next",
+		prevEl: ".s-case__arrow.is-prev",
+	},
+});
+
+const productSwiper = new Swiper("[data-swiper=product]", {
+	...optFourthCards,
+
+	navigation: {
+		nextEl: ".s-product__arrow.is-next",
+		prevEl: ".s-product__arrow.is-prev",
+	},
+});
+
+const mainSwiper = new Swiper("[data-swiper=main]", {
+	modules: [EffectFade, Navigation, Pagination, Autoplay],
+
+	slidesPerView: 1,
+	effect: 'fade',
+	loop: true,
+	autoplay: {
+		delay: 5000,
+	},
+
+	navigation: {
+		nextEl: ".s-product__arrow.is-next",
+		prevEl: ".s-product__arrow.is-prev",
+	},
+});
